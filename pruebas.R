d713e54cd1674cfd7facb988c2a60222b5e89eca
HAAGCH <hernan.alvarez10@inacapmail.cl>
2025-06-24 20:39:48 -0400

    Add files via upload

--- pruebas.R
+++ pruebas.R
@@ -9,6 +9,11 @@ library(RSQLite)
 library(dbplyr)
 library(dplyr)
 library(jsonlite)
+library(here)
+library(readr)
+library(lubridate)
+library(tidyr)
+library(stringr)
 
 #Leyendo información excel---
 
@@ -60,6 +65,72 @@ mean(inner$J2)
 #Respuesta 5
 
 cor(inner$X2, inner$J4)
+
+#Leyendo datos----
+
+complaints <- read_csv(here("data","otra_data","data_complaints.csv"))
+
+class(complaints$`Date received`)
+
+#Transformando columna a formato fecha----
+
+complaints$`Date received` <- as.Date(mdy(complaints$`Date received`))
+
+class(complaints$`Date received`)
+
+#Respuesta 1----
+
+complaints %>% filter(Product == "Student loan", `Date received` >= "2018-01-01") %>% 
+  group_by(State) %>% 
+  summarize(N=n()) %>% 
+  arrange(desc(N))
+
+#Respuesta 2----
+
+complaints$`Date sent to company` <- as.Date(mdy(complaints$`Date sent to company`))
+
+class(complaints$`Date sent to company`)
+
+complaints_mean <- complaints %>% 
+  mutate(dif_dias = `Date sent to company` - `Date received`) %>% 
+  mutate(dif_dias2 = dif_dias) %>% 
+  separate(dif_dias2, 
+    into = c("valor","dias"), sep = " ") %>% 
+  mutate(valor = as.numeric(valor)) %>% 
+  group_by(`Submitted via`) %>%
+  summarize(mean_times = mean(valor,na.rm=TRUE)) %>% 
+  mutate(mean_times = round(mean_times,1)) 
+
+complaints_mean
+
+#Respuesta 3----
+
+student <- complaints %>% filter(Product ==  "Credit card or prepaid card")
+
+str_count(student$`Consumer complaint narrative`,"student") %>% sum(na.rm=TRUE)
+
+#Respuesta 4----
+
+student_loan <- complaints %>% filter(Product == "Student loan"  )
+
+student_loan <- student_loan %>% mutate(charlength =
+  nchar(`Consumer complaint narrative`))
+
+student_loan %>% group_by(Issue) %>% summarise(charlength =
+  mean(charlength,na.rm=T)) %>% arrange(desc(charlength))
+
+#Respuesta 5----
+
+credit_card <- complaints %>% filter(Product == "Credit card or prepaid card"  )
+
+str_count(credit_card$`Consumer complaint narrative`,"account") %>% sum(na.rm=TRUE)
+
+
+
+
+
+
+
 
 
 
--- tidyverse_project.R
+++ tidyverse_project.R
@@ -9,6 +9,9 @@ install.packages("here")
 library(ProjectTemplate)
 library(here)
 library(readr)
+library(tidyverse)
+library(visdat)
+library(skimr)
 
 #Creando template del proyecto----
 
@@ -78,6 +81,381 @@ glimpse(spending)
 here()
 
 save(coverage, spending, file = here::here("data", "raw_data", "case_study_1.rda"))
+
+#Cargando los datos----
+
+load(here::here("data","raw_data", "case_study_1.rda"))
+
+coverage
+
+#Datos de los estados----
+
+library(datasets)
+data(state)
+state.name
+
+##Agregando información sobre Washington DC----
+
+state.abb <- c(state.abb, "DC")
+state.region <- as.factor(c(as.character(state.region), "South"))
+state.name <- c(state.name, "District of Columbia")
+state_data <- tibble(Location = state.name,
+  abb = state.abb,
+  region = state.region)
+state_data
+
+#Revisando datos coverage----
+
+names(coverage)
+
+#Pivot longer coverage----
+
+coverage <- coverage %>%
+  mutate(across(starts_with("20"), 
+  as.integer)) %>%  ## Convert all year-based columns to integer
+  pivot_longer(-Location,         ## Use all columns BUT 'Location'
+  names_to = "year_type", 
+  values_to = "tot_coverage")
+
+coverage
+
+#Separando columna año coverage----
+
+coverage <- coverage %>% 
+  separate(year_type, sep="__", 
+  into = c("year", "type"), 
+  convert = TRUE)
+
+coverage
+
+#Agregando abreviatura a nivel estatal coverage----
+
+coverage <- coverage %>%
+  left_join(state_data, by = "Location")
+
+coverage
+
+#Revisando datos spending----
+
+names(spending)
+
+#Pivot longer spending----
+
+spending <- spending %>%
+  pivot_longer(-Location, 
+  names_to = "year", 
+  values_to = "tot_spending")
+
+#Separando columna año spending----
+
+spending <- spending %>% 
+  separate(year, sep="__", 
+  into = c("year", "name"), 
+  convert = TRUE) %>% 
+  select(-name)
+
+spending
+
+#Uniendo coverage y spending----
+
+hc <- inner_join(coverage, spending, 
+  by = c("Location", "year"))
+
+hc
+
+#Limpiando datos de la unión----
+
+hc <- hc %>% 
+  filter(Location != "United States")
+
+table(hc$type)
+
+#Agregando columna----
+
+pop <- hc %>% 
+  filter(type == "Total") %>% 
+  select(Location, year, tot_coverage)
+
+pop
+
+hc <- hc %>% 
+  filter(type != "Total") %>% 
+  left_join(pop, by = c("Location", "year")) %>% 
+  rename(tot_coverage = tot_coverage.x, 
+  tot_pop = tot_coverage.y)
+
+hc
+
+#Calculando proporción de cobertura----
+
+hc <- hc %>% 
+  mutate(prop_coverage = tot_coverage/tot_pop) 
+
+hc
+
+#Transformando columna tot_spending de millones a dolares y agregando columna----
+
+hc <- hc %>% 
+  mutate(spending_capita = (tot_spending*1e6) / tot_pop)
+
+hc
+
+#Guardando los datos----
+
+save(hc, file = here::here("data", "tidy_data", "case_study_1_tidy.rda"))
+
+#Cargando los datos----
+
+load(here::here("data", "tidy_data", "case_study_1_tidy.rda"))
+
+#Viendo datos faltantes con Viz----
+
+vis_dat(hc)
+
+hc
+
+#Viendo % datos faltantes con Viz----
+
+vis_miss(hc)
+
+#Viendo que observaciones tienen datos faltantes----
+
+hc %>%
+  filter(is.na(tot_coverage))
+
+#Viendo tipo de variables----
+
+skim(hc)
+
+#Agrupando por años----
+
+hc %>% 
+  group_by(year) %>%
+  skim()
+
+#Gráficos exploratorios----
+
+hc %>%
+  filter(type == "Employer", 
+    year == "2013") %>% 
+  ggplot(aes(x = spending_capita, 
+    y = prop_coverage)) +
+  geom_point() + 
+  labs(x = "spending per capita",
+    y = "coverage proportion")
+
+hc %>%
+  filter(type == "Employer", 
+    year == "2013") %>% 
+  ggplot(aes(x = spending_capita, 
+    y = prop_coverage)) + 
+  geom_point() + 
+  labs(x = "spending per capita",
+    y = "coverage proportion") +
+  geom_smooth(method = "lm", col = "red")
+
+hc %>%
+  filter(type == "Employer", 
+         year == "2013") %>% 
+  ggplot(aes(x = spending_capita, 
+             y = prop_coverage)) + 
+  geom_point() + 
+  labs(x = "spending per capita",
+       y = "coverage proportion") +
+  geom_smooth(method = "lm", col = "red") + 
+  geom_text(aes(label=abb), 
+            nudge_x = 150)
+
+hc %>%
+  filter(type == "Employer", 
+    year == "2013") %>% 
+  ggplot(aes(x = spending_capita, 
+    y = prop_coverage,
+    color = region)) + 
+  geom_point() + 
+  labs(x = "spending per capita",
+    y = "coverage proportion") +
+  geom_smooth(method = "lm", col = "red") + 
+  geom_text(aes(label=abb), 
+    nudge_x = 150, 
+    show.legend = FALSE)
+
+hc %>%
+  filter(type == "Employer") %>% 
+  ggplot(aes(x = spending_capita, 
+    y = prop_coverage,
+    color = region)) + 
+  geom_point() + 
+  facet_wrap(~year) +
+  labs(x = "spending per capita",
+    y = "coverage proportion") +
+  geom_smooth(method = "lm", col = "red") + 
+  geom_text(aes(label=abb), 
+    nudge_x = 150, 
+    show.legend = FALSE)
+
+hc %>%
+  filter(year == "2013") %>% 
+  ggplot(aes(x = spending_capita, 
+    y = prop_coverage,
+    color = region)) + 
+  geom_point() + 
+  facet_wrap(~type) +
+  labs(x = "spending per capita",
+    y = "coverage proportion") +
+  geom_smooth(method = "lm", col = "red") + 
+  geom_text(aes(label=abb), 
+    nudge_x = 150, 
+    show.legend = FALSE)
+
+hc %>%
+  filter(year == "2014") %>% 
+  ggplot(aes(x = spending_capita, 
+    y = prop_coverage,
+    color = region)) + 
+  geom_point() + 
+  facet_wrap(~type) +
+  labs(x = "spending per capita",
+    y = "coverage proportion") +
+  geom_smooth(method = "lm", col = "red") + 
+  geom_text(aes(label=abb), 
+    nudge_x = 150, 
+    show.legend = FALSE)
+
+#Guardando gráficos entre cobertura y gasto----
+
+pdf(here::here("figures", "exploratory", "2013and2014_spending_and_coverage.pdf"))
+
+hc %>%
+  filter(type == "Employer") %>% 
+  ggplot(aes(x = spending_capita, 
+    y = prop_coverage,
+    color = region)) + 
+  geom_point() + 
+  facet_wrap(~year) +
+  labs(x = "spending per capita",
+    y = "coverage proportion") +
+  geom_smooth(method = "lm", col = "red") + 
+  geom_text(aes(label=abb), 
+    nudge_x = 150, 
+    show.legend = FALSE)
+
+dev.off()
+
+pdf(here::here("figures", "exploratory", "2013_coverage_type.pdf"))
+
+hc %>%
+  filter(year == "2013") %>% 
+  ggplot(aes(x = spending_capita, 
+    y = prop_coverage,
+    color = region)) + 
+  geom_point() + 
+  facet_wrap(~type) +
+  labs(x = "spending per capita",
+    y = "coverage proportion") +
+  geom_smooth(method = "lm", col = "red") + 
+  geom_text(aes(label=abb), 
+    nudge_x = 150, 
+    show.legend = FALSE)
+
+dev.off()
+
+pdf(here::here("figures", "exploratory", "2014_coverage_type.pdf"))
+
+hc %>%
+  filter(year == "2014") %>% 
+  ggplot(aes(x = spending_capita, 
+    y = prop_coverage,
+    color = region)) + 
+  geom_point() + 
+  facet_wrap(~type) +
+  labs(x = "spending per capita",
+    y = "coverage proportion") +
+  geom_smooth(method = "lm", col = "red") + 
+  geom_text(aes(label=abb), 
+    nudge_x = 150, 
+    show.legend = FALSE)
+
+dev.off()
+
+#Gráficos gastos por regiones----
+
+hc %>% 
+  ggplot(aes(x = region, 
+             y = spending_capita)) + 
+  geom_boxplot() +
+  labs(y = "spending per capita")
+
+hc %>% 
+  filter(type == "Employer") %>%
+  ggplot(aes(x = region, 
+             y = spending_capita)) + 
+  geom_boxplot(outlier.shape = NA) +
+  geom_jitter(alpha = 0.2) +
+  labs(y = "spending per capita")
+
+#Gráficos con grid----
+
+hc %>% 
+  ggplot(aes(x = spending_capita, 
+    y = prop_coverage,
+    color = region)) + 
+  geom_point() + 
+  labs(x = "spending per capita",
+    y = "coverage proportion") +
+  geom_smooth(method = "lm", col = "red") + 
+  facet_grid(year~type)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
